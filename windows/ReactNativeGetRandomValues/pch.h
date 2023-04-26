5b0b58601cd3a6d12bd546e6cd4ecce8589c01c1
Eric Rozell <erozell@outlook.com>
2023-04-26 11:44:10 -0400

    Fix issue with GetCurrentTime macro on clang builds

--- windows/ReactNativeGetRandomValues/pch.h
+++ windows/ReactNativeGetRandomValues/pch.h
@@ -2,11 +2,14 @@
 
 #define NOMINMAX
 
+#pragma push_macro("GetCurrentTime")
+#undef GetCurrentTime
 #include <unknwn.h>
 
-#include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Security.Cryptography.h>
 #include <winrt/Windows.Storage.Streams.h>
 
 #include <winrt/Microsoft.ReactNative.h>
+#pragma pop_macro("GetCurrentTime")
